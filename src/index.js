598033b681a3df8f0dd5b373d196796d2132d8ee
Radu Chirilov <radulin4a@gmail.com>
2022-04-21 11:41:08 +0300

    Promisify the ffmpeg call

--- src/index.js
+++ src/index.js
@@ -54,38 +54,49 @@ const setAudioSelect = () => {
     })
 }
 
+const serveVideo = (dataUri) => {
+    const video = $('#mp4')
+    video.src = dataUri
+}
+
 const createVideoAsync = async (videoName, audioName) => {
     let [videoBuffer, audioBuffer] = await Promise.all([getVideoBuffer(videoName), getAudioBuffer(audioName)]);
     console.log('Video buffer: ' + videoBuffer);
     console.log('Audio buffer: ' + audioBuffer);
     
-    const resBuffer = await getNewVideo(videoBuffer, audioBuffer)
-    console.log('New compiled Video buffer')
-    const blob = new Blob([resBuffer], {type: 'video/mp4'})
-    const dataUri = window.URL.createObjectURL(blob)
-    
-    const video = $('#mp4')
-    video.src = dataUri
+    getNewVideo(videoBuffer,audioBuffer)
+        .then(resBuffer => new Blob([resBuffer], {type: 'video/mp4'}))
+        .then(blob => window.URL.createObjectURL(blob))
+        .then(dataUri => { serveVideo(dataUri) })
+        .catch(err => console.error(err))
 }
 
-getNewVideo = async (videoBuffer, audioBuffer) => {
-    var result = await ffmpeg({
-        MEMFS: [
-            {name: "video.mp4", data: videoBuffer},
-            {name: "audio.wav", data: audioBuffer}
-        ],
-        arguments: ["-i", "video.mp4", "-i", "audio.wav", "-c:v", "copy", "-c:a", "aac", "output.mp4"],
-        
-        // Ignore stdin read requests.
-        stdin: function() {},
-    });
-    // Write out.gif to disk.
-    var out = result.MEMFS[0];
-    if (!out) {
-        throw new Error('Could not convert video')
-    }
-    return Uint8Array.from(out.data)
+const getNewVideo = (videoBuffer, audioBuffer) => {
+    return new Promise((resolve, reject) => {
+        // Create video
+        try{
+            var result = ffmpeg({
+                MEMFS: [
+                    {name: "video.mp4", data: videoBuffer},
+                    {name: "audio.wav", data: audioBuffer}
+                ],
+                arguments: ["-i", "video.mp4", "-i", "audio.wav", "-c:v", "copy", "-c:a", "aac", "output.mp4"],
+                
+                // Ignore stdin read requests.
+                stdin: function() {},
+            });
+            // Write out.gif to disk.
+            var out = result.MEMFS[0];
+            if (!out) {
+                throw new Error('Could not convert video')
+            }
+            resolve(Uint8Array.from(out.data))
+        } catch(err) {
+            reject(err)
+        }
+    })   
 }
+
 
 const createVideo = () => {
     const videoOption = $('#video-select').value
